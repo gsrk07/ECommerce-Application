d0872c03484ea317c0278f62a0e3e3dcdca04ec5
SriramKumar <gode.srk@gmail.com>
2025-05-25 10:49:33 +0530

    First Commit for ECommerce Project

---
+++ src/test/java/com/sriramcode/EcommerceShoppingApp/EcommerceShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriramcode.EcommerceShoppingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
