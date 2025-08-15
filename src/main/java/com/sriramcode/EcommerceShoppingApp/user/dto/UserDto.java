40e8b4095b43a5afa5afb4b79e828d650a40fcd0
SriramKumar <gode.srk@gmail.com>
2025-08-15 11:40:44 +0530

    Completed the User DTO's code

---
+++ src/main/java/com/sriramcode/EcommerceShoppingApp/user/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.sriramcode.EcommerceShoppingApp.user.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<OrderDto> orders;
+    private CartRequestDto cart;
+}
---
+++ src/main/java/com/sriramcode/EcommerceShoppingApp/user/dto/UserUpdateRequest.java
@@ -0,0 +1,9 @@
+package com.sriramcode.EcommerceShoppingApp.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateRequest {
+    private String firstName;
+    private String lastName;
+}
