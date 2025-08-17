3db68d1b8c7331fe43200a7bce0bdd20b8fe15bf
SriramKumar <gode.srk@gmail.com>
2025-08-17 19:44:28 +0530

    Added the User Exception and fixed the user controller class

---
+++ src/main/java/com/sriramcode/EcommerceShoppingApp/user/exception/AlreadyExistException.java
@@ -0,0 +1,6 @@
+package com.sriramcode.EcommerceShoppingApp.user.exception;
+
+public class AlreadyExistException extends RuntimeException{
+    public AlreadyExistException(String userAlreadyExists) {
+    }
+}
