be67d7b6569578fb11e086a59c3d7b90c5c9f5dc
SriramKumar <gode.srk@gmail.com>
2025-08-18 18:06:57 +0530

    Created the Products related model and package

---
+++ src/main/java/com/sriramcode/EcommerceShoppingApp/products/models/Image.java
@@ -0,0 +1,31 @@
+package com.sriramcode.EcommerceShoppingApp.products.models;
+
+import com.sriramcode.EcommerceShoppingApp.commons.models.BaseModel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image extends BaseModel {
+    private String fileName;
+    private String fileType;
+
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
