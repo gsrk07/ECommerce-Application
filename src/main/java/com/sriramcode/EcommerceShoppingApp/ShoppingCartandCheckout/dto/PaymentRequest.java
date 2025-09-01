dcdd971a18efa6e28bacbd9fa8647151c735d773
SriramKumar <gode.srk@gmail.com>
2025-09-01 20:52:25 +0530

    completed DTO'

---
+++ src/main/java/com/sriramcode/EcommerceShoppingApp/ShoppingCartandCheckout/dto/PaymentRequest.java
@@ -0,0 +1,8 @@
+package com.sriramcode.EcommerceShoppingApp.ShoppingCartandCheckout.dto;
+
+public class PaymentRequest {
+    private String orderId;
+    private String customerName;
+    private String phone;
+    private int amount;
+}
